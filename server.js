d4552d530cdba630df66db38d1452bc71d01dfdf
Dan Bravender <dan.bravender@gmail.com>
2011-06-12 02:10:10 +0900

    Now loads definitions from an external definition database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
-  , conjugator = require('./html/korean/conjugator');
+  , conjugator = require('./html/korean/conjugator')
+  , sqlite3 = require('sqlite3');
 
 var app = express.createServer();
 
@@ -22,13 +23,30 @@ app.configure('production', function(){
 
 app.get('/', function (req, res) {
   var infinitive = conjugator.base(req.query.infinitive || '하') + '다';
-  conjugator.conjugate(infinitive, req.query.regular, function(conjugations) {
-    res.render('index.jade', {
-      infinitive: infinitive,
-      conjugations: conjugations
+  var definitions = [];
+  show_conjugations = function() {
+    conjugator.conjugate(infinitive, req.query.regular, function(conjugations) {
+      res.render('index.jade', {
+        infinitive: infinitive,
+        conjugations: conjugations,
+        definitions: definitions
+      });
     });
-  });
+  };
+  if (db.open) {
+    select_definition.all(infinitive, function(err, results) {
+      definitions = results.map(function(x) { return x.definition });
+      show_conjugations();
+    });
+  } else {
+    show_conjugations();
+  }
 });
 
-app.listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+var select_definition;
+
+var db = new sqlite3.Database('korean-verb-database/korean-verb-definitions.sqlite', sqlite3.OPEN_READONLY, function() {
+  select_definition = db.prepare("SELECT definition FROM verbs WHERE infinitive = ?");
+  app.listen(3000);
+  console.log('Server running at http://127.0.0.1:3000/');
+});
