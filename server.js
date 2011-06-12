274f4baa991a5f3aa3649779609ee27bbf886915
Dan Bravender <dan.bravender@gmail.com>
2011-06-13 00:16:59 +0900

    Use full text definition searches to improve speed

--- server.js
+++ server.js
@@ -23,54 +23,52 @@ app.configure('production', function(){
 });
 
 app.get('/', function (req, res) {
+  var search = req.query.search;
   // redirect old requests from when the site only handled infinitive lookups
   if (req.query.infinitive) {
     return res.redirect('/?search=' + req.query.infinitive);
   }
+  if (!search) {
+    search = '하다';
+  }
   // if non-hangeul characters appear in the input search the definitions
-  if (!hangeul.is_hangeul_string(req.query.search)) {
+  if (!hangeul.is_hangeul_string(search)) {
     select_by_definition.all(
-      '%' + req.query.search + '%',
+      search + '*',
       function(err, results) {
         // redirect to the conjugation if there is only one result
         if (results.length == 1) {
-          return res.redirect('/?search=' + results[0].infinitive);
+          res.redirect('/?search=' + results[0].infinitive);
         }
         res.render('definition-search.jade', {
-          search: req.query.search,
+          search: search,
           results: results
         });
       }
     );
   } else {
-    var infinitive = conjugator.base(req.query.search || '하') + '다';
+    var infinitive = conjugator.base(search || '하') + '다';
     var verbs = [];
     var definitions = [];
     var valid_verbs = [];
-    show_conjugations = function() {
-      conjugator.conjugate(
-        infinitive,req.query.regular,
-        function(conjugations) {
-          res.render('index.jade', {
-            search: infinitive,
-            conjugations: conjugations,
-            definitions: definitions,
-            valid_verbs: valid_verbs
-          });
-        }
-      );
-    };
-    if (db.open) {
-      select_definition.all(infinitive, function(err, results) {
-        definitions = results.map(function(x) { return x.definition });
-        select_verb_type.all(infinitive, function(err, results) {
-          valid_verbs = results;
-          show_conjugations();
-        });
+    select_definition.all(infinitive, function(err, results) {
+      definitions = results.map(function(x) { return x.definition });
+      select_verb_type.all(infinitive, function(err, results) {
+        valid_verbs = results;
+        conjugator.conjugate(
+          infinitive,
+          req.query.regular,
+          function(conjugations) {
+            res.render('index.jade', {
+              search: infinitive,
+              conjugations: conjugations,
+              definitions: definitions,
+              valid_verbs: valid_verbs
+            });
+          }
+        );
       });
-    } else {
-      show_conjugations();
-    }
+    });
   }
 });
 
@@ -79,7 +77,7 @@ var select_definition;
 var db = new sqlite3.Database('korean-verb-database/korean-verbs.sqlite', sqlite3.OPEN_READONLY, function() {
   select_definition = db.prepare("SELECT definition FROM verbs WHERE infinitive = ?");
   select_verb_type = db.prepare("SELECT infinitive, verb_type FROM valid_verbs WHERE infinitive = ?");
-  select_by_definition = db.prepare("SELECT infinitive, definition FROM verbs WHERE definition LIKE ?");
+  select_by_definition = db.prepare("SELECT infinitive, definition FROM verbs WHERE definition MATCH ?");
   app.listen(3000);
   console.log('Server running at http://127.0.0.1:3000/');
 });
