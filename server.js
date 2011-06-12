7ee4309f131db90a8169ad302b726717ffa00b70
Dan Bravender <dan.bravender@gmail.com>
2011-06-12 18:39:29 +0900

    Search for English definitions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
   , conjugator = require('./html/korean/conjugator')
+  , hangeul = require('./html/korean/hangeul')
   , sqlite3 = require('sqlite3');
 
 var app = express.createServer();
@@ -22,31 +23,63 @@ app.configure('production', function(){
 });
 
 app.get('/', function (req, res) {
-  var infinitive = conjugator.base(req.query.infinitive || '하') + '다';
-  var definitions = [];
-  show_conjugations = function() {
-    conjugator.conjugate(infinitive, req.query.regular, function(conjugations) {
-      res.render('index.jade', {
-        infinitive: infinitive,
-        conjugations: conjugations,
-        definitions: definitions
-      });
-    });
-  };
-  if (db.open) {
-    select_definition.all(infinitive, function(err, results) {
-      definitions = results.map(function(x) { return x.definition });
-      show_conjugations();
-    });
+  // redirect old requests from when the site only handled infinitive lookups
+  if (req.query.infinitive) {
+    return res.redirect('/?search=' + req.query.infinitive);
+  }
+  // if non-hangeul characters appear in the input search the definitions
+  if (!hangeul.is_hangeul_string(req.query.search)) {
+    select_by_definition.all(
+      '%' + req.query.search + '%',
+      function(err, results) {
+        // redirect to the conjugation if there is only one result
+        if (results.length == 1) {
+          return res.redirect('/?search=' + results[0].infinitive);
+        }
+        res.render('definition-search.jade', {
+          search: req.query.search,
+          results: results
+        });
+      }
+    );
   } else {
-    show_conjugations();
+    var infinitive = conjugator.base(req.query.search || '하') + '다';
+    var verbs = [];
+    var definitions = [];
+    var valid_verbs = [];
+    show_conjugations = function() {
+      conjugator.conjugate(
+        infinitive,req.query.regular,
+        function(conjugations) {
+          res.render('index.jade', {
+            search: infinitive,
+            conjugations: conjugations,
+            definitions: definitions,
+            valid_verbs: valid_verbs
+          });
+        }
+      );
+    };
+    if (db.open) {
+      select_definition.all(infinitive, function(err, results) {
+        definitions = results.map(function(x) { return x.definition });
+        select_verb_type.all(infinitive, function(err, results) {
+          valid_verbs = results;
+          show_conjugations();
+        });
+      });
+    } else {
+      show_conjugations();
+    }
   }
 });
 
 var select_definition;
 
-var db = new sqlite3.Database('korean-verb-database/korean-verb-definitions.sqlite', sqlite3.OPEN_READONLY, function() {
+var db = new sqlite3.Database('korean-verb-database/korean-verbs.sqlite', sqlite3.OPEN_READONLY, function() {
   select_definition = db.prepare("SELECT definition FROM verbs WHERE infinitive = ?");
+  select_verb_type = db.prepare("SELECT infinitive, verb_type FROM valid_verbs WHERE infinitive = ?");
+  select_by_definition = db.prepare("SELECT infinitive, definition FROM verbs WHERE definition LIKE ?");
   app.listen(3000);
   console.log('Server running at http://127.0.0.1:3000/');
 });
